1f26fd99d5f2d3a153cc4dcf8d535f02c82cdcd9
Augusto Quisquinay <augustoacual@gmail.com>
2023-09-10 11:08:16 -0600

    Getting Started with Animated List

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container">
+    <users-list />
+  </div>
+  <div class="container">
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
     <transition
-      :css="false"
+      name="para"
       @before-enter="beforeEnter"
       @enter="enter"
       @after-enter="afterEnter"
@@ -35,7 +38,10 @@
 </template>
 
 <script>
+import UsersList from './components/UsersList.vue'
+
 export default {
+  components: { UsersList },
   data() {
     return {
       animatedBlock: false,
